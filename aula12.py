d7cbb76a7b5ce160a88ddb964b9e84c51b593a50
Vanessa Silva <vannisban@hotmail.com>
2025-09-02 19:38:42 -0300

    Exercise Progress & Updates
    Exercises organized by modules.

---
+++ aula12.py
@@ -0,0 +1,12 @@
+nome = 'Vanessa Silva'
+altura = 1.70
+peso = 82
+imc = peso / altura ** 2
+
+print(nome, 'tem', altura, 'de altura,',)
+print('pesa', peso, 'quilos e seu imc é',)
+print(imc)
+
+# Vanessa tem 1.70 de altura,
+# pesa 82 quilos e seu IMC é
+# 28.373702422145332
